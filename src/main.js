33d72007316ec6d8d9c0c819eadff69182f8ed44
murong <murong@yscredit.com>
2021-11-11 17:06:20 +0800

    docs: init a folder

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import App from './App.vue';
-import Vue from 'vue';
-import router from './router/index.js';
-
-new Vue({
-  router,
-  render: (h) => h(App),
-}).$mount("#app");
-
--- test/main.js
+++
@@ -1,4 +0,0 @@
-// console.log(${ npm_config_first });
-console.log('hello world');
-
-console.log(process.env.npm_config_my)
