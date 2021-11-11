5197f9d6fd5f04af741d4ca778bd2d17ae5ed4b8
goodTom <845254712@qq.com>
2021-11-11 23:50:34 +0800

    docs: webpack set up

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
 require('./assets/main.css');
-const show = require('./utils/index');
+// const show = require('./utils/index');
+const show = require('utils');
+
+
+
 show('webpack');
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,9 @@ const path = require("path");
 // extract-text-webpack-plugin 已弃用
 // const ExtractTextPlugin = require('extract-text-webpack-plugin'); 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-  
+
+// webpack 中的配置都有默认值，从而实现了开箱即用，零配置的特点
+
 module.exports = {
   mode: 'none',
   entry: './src/main.js',
@@ -26,5 +28,32 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: `[name].css`
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      // 使用 别名代替一些绝对路径，方便路径管理
+      utils: path.resolve(__dirname, 'src/utils')
+    },
+    // 设定 从npm 导包时按何种字段导入模块
+    mainFields: ['browser', 'module', 'main'],
+    // 解析目录时要使用的文件名，省略文件名
+    mainFiles: ['index'],
+    // 解析未带后缀的文件时的默认顺序
+    extensions: ['.js'],
+    //  配置 webpack 去哪些目录下找第三方包
+    modules: ['node_modules'],
+  },
+  //  只用通过 DevServer 启动webpack 此配置才会奏效
+  devServer: {
+    //  是否开启g-zip 压缩模式
+    compress: true,
+    //  服务监听地址
+    host: '0.0.0.0',
+    //  服务器 端口号
+    port: 8089
+  },
+  // 是否生产source-map
+  devtool: 'source-map',
+  // 目标对象
+  target: 'web',
 };
